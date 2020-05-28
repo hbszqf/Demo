a2cce7c5651688bd4371bc76aedd7883331532ec
hbszqf <523184236@qq.com>
2020-05-28 18:53:14 +0800

    Fairy测试 以及 c# 启动部分

---
+++ Assets/Scripts/Game/Const/AppConst.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AppConst 
+{
+    public const string DynamicAssetPathPrefix = "Assets/Res/Dynamic/";//读取Dynamic Asset的文件路径前缀
+    public const string FairyGUIEditorOutPut =   DynamicAssetPathPrefix + "FairyGUI/";//读取Dynamic Asset的文件路径前缀
+}
