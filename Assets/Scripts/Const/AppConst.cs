407be0a337af0d0eaac8bb2a6938e0d883b16673
hbszqf <523184236@qq.com>
2020-05-29 14:38:52 +0800

    修改项目结构

---
+++ Assets/Scripts/Const/AppConst.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AppConst 
+{
+    public const string DynamicAssetPathPrefix = "Assets/Res/Dynamic/";//读取Dynamic Asset的文件路径前缀
+    public const string FairyGUIEditorOutPut =   DynamicAssetPathPrefix + "FairyGUI/";//读取Dynamic Asset的文件路径前缀
+    static public  bool  LuaBundleMode = false;    // 是否使用luaab
+
+
+    public static string FrameworkRoot
+    {
+        get
+        {
+            return Application.dataPath + "/3rd/tolua";
+        }
+    }
+
+
+    public static string LuaRoot
+    {
+        get
+        {
+            return Application.dataPath + "/Lua";
+        }
+    }
+
+    public static string ToLuaRoot
+    {
+        get
+        {
+            return FrameworkRoot + "/ToLua/Lua";
+        }
+    }
+}
