98ed2d203c22c911c7289253d1d64a5f5f0b63c3
qin.fang <QinFang@20201123SPEDRFH>
2021-05-07 18:20:52 +0800

    fgui editor 更新

---
+++ Dependency/FairyGUI-Editor/FairyGUI-Editor 2.app/Contents/Frameworks/Override_OSX.framework/Versions/A/Headers/Override_OSX.h
@@ -0,0 +1,21 @@
+//
+//  Override_OSX.h
+//  Override_OSX
+//
+//  Created by guzhu on 2020/4/28.
+//  Copyright © 2020 fairygui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Override_OSX.
+FOUNDATION_EXPORT double Override_OSXVersionNumber;
+
+//! Project version string for Override_OSX.
+FOUNDATION_EXPORT const unsigned char Override_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Override_OSX/PublicHeader.h>
+
+
+@interface Override_OSX_Loader : NSObject
+@end
